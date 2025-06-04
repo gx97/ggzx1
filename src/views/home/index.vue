1220d2bcac108aa41f34cf3a7b5bd4638230854b
guxu@wondersgroup.com <guxu@wondersgroup.com>
2025-06-04 16:18:07 +0800

    首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,53 @@
 <template>
-  <div>
-    <h1>我是一级路由；展示登陆成功后的数据</h1>
+  <el-card>
+    <div class="box">
+      <img class="avatar" :src="userStore.avatar">
+      <div class="bottom">
+        <h3 class="title">{{ getTime() }}好,{{ userStore.username }}</h3>
+        <p class="subTitle">ggzw运营平台</p>
+      </div>
+    </div>
+  </el-card>
+  <div class="bottoms">
+    <svg-icon name="welcome" width="500px" height="500px"></svg-icon>
   </div>
 </template>
 
 <script setup lang="ts">
+import useUserStore from '@/stores/modules/user';
+import { getTime } from '@/utils/time';
+
+let userStore = useUserStore();
+
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box {
+  display: flex;
+
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+
+  .bottom {
+    margin-left: 20px;
+    .title {
+      font-size: 40px;
+      font-weight: 900;
+      margin-bottom: 30px;
+    }
+    .subTitle{
+      font-style: italic;
+      color: skyblue;
+    }
+  }
+}
+
+.bottoms{
+  display: flex;
+  justify-content: center;
+}
+</style>
